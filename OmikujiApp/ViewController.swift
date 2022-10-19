092178f9b17d1e7c3dbaa5464b04e7f93ced7ee4
Yoshihiko Higa <yoshi1osukar@gmail.com>
2022-10-19 17:55:04 +0900

    Initial Commit

---
+++ OmikujiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OmikujiApp
+//
+//  Created by osukar on 2022/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
